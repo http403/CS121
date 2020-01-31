8019c75bd6932db3d28173d13a0d6f565304f4d0
Hartman Tam <unconfigured@null.spigotmc.org>
2020-01-30 20:52:06 -0800

    PracticeWithFunctionsTestCases: added comments and execution result

--- PracticeWithFunctionsTestCases/main.py
+++ PracticeWithFunctionsTestCases/main.py
@@ -10,6 +10,7 @@ from functions import (
 )
 # -----main-----
 if __name__ == '__main__':
+    # little hack to make unit test uncommented as I'm using PyCharm
     if os.getenv("UNIT_TEST"):
         # IGNORE THIS UNTIL YOU'VE FINISHED YOUR SUBMISSION, THEN YOU CAN TRY TO UNCOMMENT AND RUN THESE
         import unittest
@@ -30,10 +31,14 @@ if __name__ == '__main__':
 
         unittest.TextTestRunner().run(suite)
     else:
+        # Actual program runs
+
+        # Coin flip
         print("What will I get?")
         for i in range(3):
             coin_flip()
 
+        # emojis
         print()
         print("Can we do emojis?")
         print(emojify("pig"), emojify("pizza"))
@@ -45,23 +50,59 @@ if __name__ == '__main__':
             emojied_story = emojied_story + emojify(w) + " "
         print(emojied_story)
 
+        # math hypotenuse
         print()
         print("How about math?")
         print("sqrt(3^2 + 4^2) = {}".format(hypotenuse(3, 4)))
         print("How about with floats?")
         print("sqrt(6.4^2 + 9.3^2) = {:.2f}".format(hypotenuse(6.4, 9.3)))
 
+        # sorting hat
         print()
         print("Sorting Hat? What's that?")
         for i in range(4):
             print(sorting_hat())
 
+        # weekly pay
         print()
         print("With all of these, what's my pay?")
         print("If I worked 25 hours with $7.5/hr and I will get ${:.2f}/week".format(weekly_pay(25, 7.5)))
         print("But I actually worked 50 hours with $0/hr. So my pay is ${:.2f}. Sad".format(weekly_pay(50, 0)))
 
+        # letter grade
         print()
         print("No pay isn't a problem. Give me a grade.")
         print("My Grade is {}.".format(letter_grade(50)))
         print("You must be kidding me. My grade should be {}.".format(letter_grade(105)))
+
+"""
+What will I get?
+Coin flip result is heads.
+Coin flip result is heads.
+Coin flip result is tails.
+
+Can we do emojis?
+🐽 🍕
+More complicated?
+🐽 gave 🦏 a 🎈 for his birthday. they ate 🍕 and were happy. 
+
+How about math?
+sqrt(3^2 + 4^2) = 5.0
+How about with floats?
+sqrt(6.4^2 + 9.3^2) = 11.29
+
+Sorting Hat? What's that?
+Hufflepuff
+Ravenclaw
+Slytherin
+Hufflepuff
+
+With all of these, what's my pay?
+If I worked 25 hours with $7.5/hr and I will get $187.50/week
+ALLERT: OVERTIME
+But I actually worked 50 hours with $0/hr. So my pay is $0.00. Sad
+
+No pay isn't a problem. Give me a grade.
+My Grade is F.
+You must be kidding me. My grade should be DISQUALIFIED. No cheating.
+"""
